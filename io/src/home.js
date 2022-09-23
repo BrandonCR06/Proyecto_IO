00b76e6315773a5092082be5f13c36a679fc39ae
BrandonCR06 <rjbrandon0@gmail.com>
2022-09-22 23:55:22 -0600

    Bootstrap

--- io/src/home.js
+++ io/src/home.js
@@ -4,10 +4,12 @@ function Home() {
   return (
     <body class="">
       <div class= "">
-        
-        
-        <h1>Hola Mundo</h1>
-
+        <h1 class=''>Hola Mundo</h1>
+        <div class="btn-group-vertical">
+          <button type="button" class="btn btn-secondary btn-lg" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+            Algoritmo
+          </button>
+        </div>
 
       </div>
     </body>
