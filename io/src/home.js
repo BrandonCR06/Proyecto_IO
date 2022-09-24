30b41c31adc4cca839d11e42951d0cecc4d10785
Ricardo Soto <ricardosoto@MacBook-Air-de-Ricardo.local>
2022-09-24 00:46:13 -0600

    Botones y titulo

--- io/src/home.js
+++ io/src/home.js
@@ -3,18 +3,39 @@ import './App.css';
 function Home() {
   return (
     <body>
-      <h1 class='text-center text-white'>Hola Mundo</h1>
+      <h1 class='text-center text-white'>Visualizador de algoritmos</h1>
       <br></br>
       <div class="text-center"></div>
       <br></br>
-      <div class="text-center">
-        <div class="btn-group btn-group-lg btn-group-vertical">
-        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
-          Algoritmo
+      <div class="text-center">        
+        <div class="btn-group-lg btn-group-vertical">          
+        <button type="button" class="btn btn-secondary btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Rutas más cortas 
         </button>
-        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
-          Algoritmo
+        <li > </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Problema de la mochila
         </button>
+        <li > </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Reemplazo de equipos
+        </button>
+        <li > </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Árboles Binarios de Búsqueda Óptimos
+        </button>
+        <li > </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Series Deportivas
+        </button>
+        <li > </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+            Multiplicación de Matrices 
+        </button>
+        <li> </li>
+        <button type="button" class="btn btn-secondary btn-block btn-outline-white" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Salir del Programa
+        </button>        
         </div>
       </div>
     </body>
