3dc96894e30138182a0b577da1f7411854610cc9
BrandonCR06 <rjbrandon0@gmail.com>
2022-09-23 18:15:58 -0600

    Fondo/Estructura

--- io/src/home.js
+++ io/src/home.js
@@ -2,15 +2,20 @@ import './App.css';
 
 function Home() {
   return (
-    <body class="">
-      <div class= "">
-        <h1 class=''>Hola Mundo</h1>
-        <div class="btn-group-vertical">
-          <button type="button" class="btn btn-secondary btn-lg" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
-            Algoritmo
-          </button>
+    <body>
+      <h1 class='text-center text-white'>Hola Mundo</h1>
+      <br></br>
+      <div class="text-center"></div>
+      <br></br>
+      <div class="text-center">
+        <div class="btn-group btn-group-lg btn-group-vertical">
+        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Algoritmo
+        </button>
+        <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Algoritmo bla bla bla">
+          Algoritmo
+        </button>
         </div>
-
       </div>
     </body>
   );
